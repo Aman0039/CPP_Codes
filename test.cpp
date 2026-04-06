b55f044cdd438fe0206844b045ce658944aaf9bd
Aman0039 <pathakaman0532gmail.com>
2026-04-06 17:04:53 +0530

    first cpp code

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World" << endl;
+    return 0;
+}
+
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int a;
+//     int b;
+
+//     cin >> a;
+//     cin >> b;
+
+//     cout << a + b << endl;
+//     return 0;
+// }
+
